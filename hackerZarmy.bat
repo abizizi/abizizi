c9add64dcfb15bb0ef62479696c6067ca8cc4e2f
abizizi <76852969+abizizi@users.noreply.github.com>
2021-11-16 19:38:52 +0330

    matrix.bat

---
+++ hackerZarmy.bat
@@ -0,0 +1,17 @@
+@echo off
+color a
+:start
+echo hacked
+echo %random%%random% %random%%random% %random%%random% %random%%random% %random%%random%
+@color 2
+echo %random%%random% %random%%random% %random%%random% %random%%random% %random%%random%
+color 7
+echo %random%%random% %random%%random% %random%%random% %random%%random% %random%%random%
+color 4
+echo %random%%random% %random%%random% %random%%random% %random%%random% %random%%random%
+color 9
+echo %random%%random% %random%%random% %random%%random% %random%%random% %random%%random%
+color 5
+echo %random%%random% %random%%random% %random%%random% %random%%random% %random%%random%
+goto start
+pause
